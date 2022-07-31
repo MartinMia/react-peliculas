06a328f44c36db1a173747d01eba2fa246bb8bea
Martín Miadosqui <martinmiadosqui@gmail.com>
2022-07-31 18:37:30 -0300

    Clase 36: Expresiones en JSX

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,21 @@ import './App.css';
 
 function App() {
   
+  const subtitulo = "Subtitulo"
+
+  const duplicar = (valor:number) => valor*2;
+
+  const imagenURL ="https://www.pngfind.com/pngs/m/685-6854994_react-logo-no-background-hd-png-download.png";
+
   return (
     <div>
       <h1>Hola Mundo!</h1>
-      <h3>Subtitulo</h3>
+      <h3>{subtitulo.toUpperCase()}</h3>
+      <h4>El doble de 3 es {duplicar(3)}</h4>
+      <img src={imagenURL} alt="logo react"/>
+      <div>
+        <input type="checkbox" /> Este es un checkbox
+      </div>
     </div>
   );
 }
