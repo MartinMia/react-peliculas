8ae67c2a505b4de889a7d323d5aafe2604bdaf63
Martín Miadosqui <martinmiadosqui@gmail.com>
2022-08-06 20:19:52 -0300

    Clase 54: Limpieza

--- src/App.tsx
+++ src/App.tsx
@@ -1,81 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
-import FormularioTexto from './FormularioTexto';
-import MostrarTexto from './MostrarTexto';
-import EjemploUseEffect from './EjemploUseEffect';
-import ValorContext from './ValorContext';
-import Abuelo from './Abuelo';
-import ContenidoDinamico from './ContenidoDinamico';
-import ErrorBoundary from './ErrorBoundary';
-
 function App() {
-
-  const [texto, setTexto]= useState('Valor por defecto')
-  const [checked, setChecked]= useState(true)
-
-  //const imagenURL ="https://www.pngfind.com/pngs/m/685-6854994_react-logo-no-background-hd-png-download.png";
-
-  //const manejarClick = () => console.log('click');
-  
-  const manejarKeyUp = (textoInput: string) =>{
-     console.log(textoInput);
-     setTexto(textoInput);
-  }
-
-  //const parteInter = <EjemploReloj/>
-
-  /* const estilo ={
-    backgroundColor:'red',
-    width:'50px',
-    height:'50px',
-    marginLeft:'1rem'
-  } */
-
-  /* const parteInf=<div style={estilo}></div>
-  */
-  const calificaciones=[
-    {nom:'Martin',calificacion:75},
-    {nom:'Julita',calificacion:85},
-    {nom:'Ricardo',calificacion:-1}
-  ]
 
   return (
     <>
-      <h1 className="rojo">Hola Mundo!</h1>
-
-      {calificaciones.map(cal =>
-       <ErrorBoundary key={cal.nom}>
-         <ContenidoDinamico  {...cal} />
-      </ErrorBoundary>
-      )}
-
-      {/* <ValorContext.Provider value={texto}>
-        <Abuelo />
-      </ValorContext.Provider> */}
-
-      {/* <div>
-        <input type="checkbox"  
-        onChange={(e) => setChecked(e.currentTarget.checked)}
-        checked={checked}/> Mostrar componente useEffect
-      </div>
-
-      {checked ? <EjemploUseEffect /> : null}  */}
-              
-
-      <button 
-        onMouseEnter={() => console.log('entrando')}  
-        onClick={()=> console.log('click desde el boton')}>Clickeame
-      </button>
       
-      <FormularioTexto manejarKeyUp={(e: string) => manejarKeyUp(e)}/>
-
-      <MostrarTexto texto={texto}/>     
-
-      <div>
-        <input type="checkbox"  
-        onChange={(e) => setChecked(e.currentTarget.checked)}
-        checked={checked}/> Este es un checkbox
-      </div>
     </>
   );
 }
