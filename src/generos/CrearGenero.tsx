c8db0f2c756794079495f1cc7af5cc1ae50cbaa5
Martín Miadosqui <martinmiadosqui@gmail.com>
2022-08-12 11:14:26 -0300

    Clase 66: useHistory

--- src/generos/CrearGenero.tsx
+++ src/generos/CrearGenero.tsx
@@ -1,5 +1,13 @@
+import { useHistory } from "react-router-dom"
+import Button from "../utils/Button";
+
 export default function CrearGenero(){
+    const history = useHistory();
     return(
-        <>Crear Género</>
+        <>
+            <h3>Crear Género</h3>
+            <Button onClick={() => history.push('generos')}>Salvar</Button>
+        </>
+        
     )
 }
