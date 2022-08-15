18afd6559799d2b95e8ffc221805acb06f021915
Martín Miadosqui <martinmiadosqui@gmail.com>
2022-08-15 11:12:30 -0300

    Clase 78: Formulario Actores

---
+++ src/actores/FormularioActores.tsx
@@ -0,0 +1,42 @@
+import { Form, Formik, FormikHelpers } from "formik";
+import { Link } from "react-router-dom";
+import Button from "../utils/Button";
+import FormGroupText from "../utils/FormGroupText";
+import { actorCreacionDTO } from "./actores.model";
+import * as Yup from 'yup';
+
+export default function FormularioActores(props: formularioActoresProps){
+    return(
+        
+        <Formik
+            initialValues={props.modelo}
+            onSubmit={props.onSubmit}
+            validationSchema={Yup.object({
+                nombre: Yup.string().required('Este campo es requerido').primeraLetraMayuscula()
+            })}
+        >
+
+            {(formikProps) => (
+                <Form>
+
+                    <FormGroupText campo="nombre" label="Nombre"/>
+
+                    <Button disabled={formikProps.isSubmitting}
+                            type="submit"
+                    >
+                        Salvar
+                    </Button>
+
+                    <Link className="btn btn-secondary" to="/actores">Cancelar</Link>
+                </Form>
+            )}
+
+        </Formik>
+
+    )
+}
+
+interface formularioActoresProps{
+    modelo: actorCreacionDTO;
+    onSubmit(valores: actorCreacionDTO, acciones: FormikHelpers<actorCreacionDTO>): void;
+}
