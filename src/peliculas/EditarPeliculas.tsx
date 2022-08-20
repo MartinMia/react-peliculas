acc38c98742e36a637f044e9578cfed6dc0536c6
Martín Miadosqui <martinmiadosqui@gmail.com>
2022-08-20 13:16:56 -0300

    Clase 85: Formulario Peliculas

--- src/peliculas/EditarPeliculas.tsx
+++ src/peliculas/EditarPeliculas.tsx
@@ -1,7 +1,14 @@
+import FormularioPeliculas from "./FormularioPeliculas";
+
 export default function EditarPeliculas(){
     return(
         <>
             <h3>Editar Pelicula</h3>
+            <FormularioPeliculas
+                modelo={{titulo:'Rampage',enCines: true, trailer:'url',
+            fechaLanzamiento: new Date('2019-01-01T00:00:00')}}
+                onSubmit={valores => console.log(valores)}
+            />
         </>
     )
 } 
