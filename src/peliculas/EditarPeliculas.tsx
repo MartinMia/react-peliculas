f1bfaa1c38f05ca81ad1bcc0a9a1a5a6810fb69a
Martín Miadosqui <martinmiadosqui@gmail.com>
2022-08-21 18:40:40 -0300

    Clase 86: Selector Generos

--- src/peliculas/EditarPeliculas.tsx
+++ src/peliculas/EditarPeliculas.tsx
@@ -1,14 +1,30 @@
+import { generoDTO } from "../generos/generos.model";
 import FormularioPeliculas from "./FormularioPeliculas";
 
 export default function EditarPeliculas(){
+
+    const generosNoSeleccionados: generoDTO[]=[
+        
+        {id:2, nombre:'Drama'}
+        
+    ]
+
+    const generosSeleccionados: generoDTO[]=[
+        {id:1, nombre:'Accion'},
+        
+        {id:3, nombre:'Comedia'}
+    ]
+
     return(
         <>
             <h3>Editar Pelicula</h3>
-            <FormularioPeliculas
+            <FormularioPeliculas 
+                generosNoSeleccionados={generosNoSeleccionados}
+                generosSeleccionados={generosSeleccionados}
                 modelo={{titulo:'Rampage',enCines: true, trailer:'url',
-            fechaLanzamiento: new Date('2019-01-01T00:00:00')}}
-                onSubmit={valores => console.log(valores)}
-            />
+                fechaLanzamiento: new Date('2019-01-01T00:00:00')}}
+                onSubmit={valores => console.log(valores)
+                }/>
         </>
     )
 } 
