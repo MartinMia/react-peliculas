8d44b0714f74e4a7fd67c21b7d38c35e96a82298
Martín Miadosqui <martinmiadosqui@gmail.com>
2022-08-22 00:23:45 -0300

    Clase 111: Axios y Var de Ambiente

--- src/generos/IndiceGeneros.tsx
+++ src/generos/IndiceGeneros.tsx
@@ -1,6 +1,17 @@
+import axios,{AxiosResponse} from 'axios';
+import { useEffect } from "react";
 import { Link } from "react-router-dom";
+import { urlGeneros } from '../utils/endpoint';
+import { generoDTO } from './generos.model';
 
 export default function IndiceGeneros(){
+    useEffect(() => {
+        axios.get(urlGeneros)
+            .then((respuesta: AxiosResponse<generoDTO[]>) =>{
+                console.log(respuesta.data);
+            })
+    },[])
+
     return(
         <>
             <h3>Indice Generos</h3>
