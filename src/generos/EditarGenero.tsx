b8dbecd9d682d112e02f7aa9e8b67365d42badc7
Martín Miadosqui <martinmiadosqui@gmail.com>
2022-08-23 10:24:45 -0300

    Clase 122: Generalizando Componente Edicion

--- src/generos/EditarGenero.tsx
+++ src/generos/EditarGenero.tsx
@@ -1,46 +1,22 @@
-import axios, { AxiosResponse } from "axios";
-import React, { useEffect, useState } from "react";
-import { useHistory, useParams } from "react-router-dom"
-import Cargnado from "../utils/Cargando";
+import EditarEntidad from "../utils/EditarEntidad";
 import { urlGeneros } from "../utils/endpoint";
-import MostrarErrores from "../utils/MostrarErrores";
 import FormularioGeneros from "./FormularioGeneros";
-import { generoCreacionDTO, generoDTO } from "./generos.model";
+import { generoDTO,generoCreacionDTO } from "./generos.model";
 
 export default function EditarGenero(){
 
-    const {id}: any = useParams(); 
-    const [genero, setGenero] = useState<generoDTO>();
-    const [errores, setErrores] = useState<string[]>([]);
-    const history = useHistory();
-
-    useEffect(() =>{
-        axios.get(`${urlGeneros}/${id}`)
-        .then((respuesta: AxiosResponse<generoDTO>) => {
-            setGenero(respuesta.data);
-        })
-    })
-
-    async function editar(generoEditar: generoCreacionDTO){
-        try{
-            await axios.put(`${urlGeneros}/${id}`,generoEditar);
-            history.push('/generos');
-        }
-        catch(error){
-            setErrores(error.response.data)
-        }
-    }
-
     return(
         <>
-            <h3>Editar Género</h3>
-            <MostrarErrores errores={errores}/>
-            {genero ? <FormularioGeneros modelo={genero}
-                onSubmit={async valores => {
-                    await editar(valores)
+
+            <EditarEntidad<generoCreacionDTO, generoDTO>
+                url={urlGeneros} urlIndice="/generos" nombreEntidad="Géneros">
+                {(entidad, editar) =>  
+                <FormularioGeneros modelo={entidad}
+                    onSubmit={async valores => {
+                        await editar(valores)
                 }}
-            /> : <Cargnado /> } 
-            
+            />}
+            </EditarEntidad>   
         </>
     )
 }
