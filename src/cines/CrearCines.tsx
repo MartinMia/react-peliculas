51ced36a76b513a6313d44c4f874212dad528432
Martín Miadosqui <martinmiadosqui@gmail.com>
2022-08-23 18:55:46 -0300

    Clase 131: Crear Cines

--- src/cines/CrearCines.tsx
+++ src/cines/CrearCines.tsx
@@ -1,12 +1,33 @@
+import axios from "axios";
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
+import { urlCines } from "../utils/endpoint";
+import MostrarErrores from "../utils/MostrarErrores";
+import { cineCreacionDTO } from "./cines.model";
 import FormularioCines from "./FormularioCines";
 
 export default function CrearCines(){
+
+    const history = useHistory();
+    const[errores, setErrores] = useState<string[]>([]);
+
+    async function crear(cine: cineCreacionDTO){
+        try{
+            await axios.post(urlCines, cine);
+            history.push('/cines');
+        }
+        catch(error){
+            setErrores(error.response.data);
+        }
+    }
+
     return(
         <>
             <h3>Crear Cine</h3>
+            <MostrarErrores errores={errores}/>
             <FormularioCines
                 modelo={{nombre:''}}
-                onSubmit={valores => console.log(valores)}
+                onSubmit={async valores => await crear(valores)}
             />
         </>
     )
