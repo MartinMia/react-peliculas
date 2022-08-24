1d090448a80d4b3f8182700c313c1db040e0030e
Martín Miadosqui <martinmiadosqui@gmail.com>
2022-08-23 21:23:09 -0300

    Clase 137: Selector Actores

--- src/actores/TypeAheadActores.tsx
+++ src/actores/TypeAheadActores.tsx
@@ -1,37 +1,29 @@
+import axios, { AxiosResponse } from "axios";
 import { useState } from "react";
-import { Typeahead } from "react-bootstrap-typeahead";
+import { AsyncTypeahead } from "react-bootstrap-typeahead";
 import { ReactElement } from "react-markdown/lib/react-markdown";
+import { urlActores } from "../utils/endpoint";
 import { actorPeliculaDTO } from "./actores.model";
 
 export default function TypeAheadActores(props: typeAheadActoresProps){
 
-    const actores: actorPeliculaDTO[] =[
-        {
-            id:1, 
-            nombre:'Dwayne Johnsonn',
-            personaje:'', 
-            foto:'https://m.media-amazon.com/images/M/MV5BMTkyNDQ3NzAxM15BMl5BanBnXkFtZTgwODIwMTQ0NTE@._V1_UX214_CR0,0,214,317_AL_.jpg'
-        },
-
-        {
-            id:2, 
-            nombre:'Scarlett Johansson',
-            personaje:'', 
-            foto:'https://m.media-amazon.com/images/M/MV5BMTM3OTUwMDYwNl5BMl5BanBnXkFtZTcwNTUyNzc3Nw@@._V1_UY317_CR23,0,214,317_AL_.jpg'
-        },
-
-        {
-            id:3, 
-            nombre:'Ryan Gosling',
-            personaje:'', 
-            foto:'https://m.media-amazon.com/images/M/MV5BMTQzMjkwNTQ2OF5BMl5BanBnXkFtZTgwNTQ4MTQ4MTE@._V1_UY317_CR18,0,214,317_AL_.jpg'
-        }
-    ]
+    const [estaCargando, setEstaCargando] = useState(false); 
+    const [opciones, setOpciones] = useState<actorPeliculaDTO[]>([]);
 
     const seleccion: actorPeliculaDTO[] = []
 
     const [elementoArrastrado, setElementoArrastrado] =
         useState<actorPeliculaDTO | undefined>(undefined)
+
+    function manejarBusqueda(query: string){
+        setEstaCargando(true);
+
+        axios.get(`${urlActores}/buscarPorNombre/${query}`)
+            .then((respuesta: AxiosResponse<actorPeliculaDTO[]>)=>{
+                setOpciones(respuesta.data)
+                setEstaCargando(false);
+            })
+    }
 
     function manejarDragStart(actor: actorPeliculaDTO){
         setElementoArrastrado(actor);
@@ -59,7 +51,7 @@ export default function TypeAheadActores(props: typeAheadActoresProps){
         <>
         
             <label>Actores</label>
-            <Typeahead
+            <AsyncTypeahead
                 id="typeahead"
                 onChange={actores => {
                     if(props.actores.findIndex(x => x.id === actores[0].id) === -1){
@@ -67,9 +59,11 @@ export default function TypeAheadActores(props: typeAheadActoresProps){
                     }
                 }}    
 
-                options={actores}
+                options={opciones}
                 labelKey={actor => actor.nombre}
-                filterBy={['nombre']}
+                filterBy={() => true}
+                isLoading={estaCargando}
+                onSearch={manejarBusqueda}
                 placeholder="Escriba el nombre del actor..."
                 minLength={2}
                 flip={true}
